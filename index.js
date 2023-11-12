6446f2d9caff8110c26f897b700aee87860168c5
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-12 06:32:13 +0100

    Implement PATCH /posts/:id

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method"));
 
 // Universally Unique Identifier
 // to create unique id
@@ -45,6 +48,30 @@ app.post("/posts", (req, res) => {
   let { username, content } = req.body;
   let id = uuidv4(); // use to generate unique id
   posts.unshift({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  res.render("show.ejs", { post });
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  res.render("edit.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  post.content = newContent;
   res.redirect("/posts");
 });
 
